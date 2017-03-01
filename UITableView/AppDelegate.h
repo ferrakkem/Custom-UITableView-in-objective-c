287b44b2b89d35dd2d89a26e0a466736c3b070b9
sabrinatuli <Btrac@mac-mini.local>
2017-03-01 12:18:41 +0600

    Initial Commit

---
+++ UITableView/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  UITableView
+//
+//  Created by Btrac on 3/1/17.
+//  Copyright © 2017 Ferrakkem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ UITableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableView
+//
+//  Created by Btrac on 3/1/17.
+//  Copyright © 2017 Ferrakkem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
